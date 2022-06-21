d1ce2534ebe8c2eeda39ab6e09b0549f7383646a
Maks Burenko <Cherniymaks@yandex.ru>
2022-06-21 11:44:12 +0300

    123

--- src/IO4/Main.java
+++ src/IO4/Main.java
@@ -1,7 +1,6 @@
 package IO4;
 
 import java.io.*;
-import java.text.DecimalFormat;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -9,7 +8,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         String text = "";
-        String finalSum = "";
         double sum = 0.000000;
 
         InputStream is = System.in;
