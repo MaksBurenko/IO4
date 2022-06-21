91acdd8fb4d5544116f3314b1a991850d876f630
Maks Burenko <Cherniymaks@yandex.ru>
2022-06-21 11:20:40 +0300

    Initial commit

---
+++ src/IO4/Main.java
@@ -0,0 +1,42 @@
+package IO4;
+
+import java.io.*;
+import java.text.DecimalFormat;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        String text = "";
+        String finalSum = "";
+        double sum = 0.000000;
+
+        InputStream is = System.in;
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader reader = new BufferedReader(isr);
+
+        OutputStream os = System.out;
+        OutputStreamWriter osw = new OutputStreamWriter(os);
+        BufferedWriter bw = new BufferedWriter(osw);
+
+
+        while ((text = reader.readLine()) != null) {
+            StringTokenizer st = new StringTokenizer(text);
+            while (st.hasMoreTokens()) {
+                try {
+                    double d = Double.parseDouble(st.nextToken());
+                    sum = sum + d;
+
+                } catch (NumberFormatException e) {
+                }
+            }
+        }
+        String str = String.format("%6f", sum);
+        bw.write(str);
+
+        reader.close();
+        bw.flush();
+        bw.close();
+    }
+}
